a44bf33ca08f0b2e8dcf55823cb3b5b5004b6791
Blaine Furman <bfurman@hawaii.edu>
2019-08-11 18:25:50 -1000

    Fixed but not
    This is just making a backup of the previous file and I need to fix the char to BYTE file to send to the board. Also, there is a problem that happens when there is a exception thrown, when the getReg crashes.

--- Adept2Test/Adept2Test.cpp
+++ Adept2Test/Adept2Test.cpp
@@ -1,23 +1,24 @@
 /************************************************************************/
 /*																		*/
-/*  DaciDemo.cpp  --  DACI Demo Main Program							*/
+/*  DeppDemo.cpp  --  DEPP DEMO Main Program							*/
 /*																		*/
 /************************************************************************/
 /*  Author:	Aaron Odell													*/
 /*  Copyright:	2010 Digilent, Inc.										*/
 /************************************************************************/
 /*  Module Description: 												*/
-/*		DACI Demo is a project designed to demonstrate how				*/
-/*		to send and recieve data asynchronously on an I/O				*/
-/*		Explorer board using the DACI module of the Adept SDK.			*/
+/*		DEPP Demo demonstrates how to transfer data to and from			*/
+/*		a Digilent FPGA board using the DEPP module of the Adept		*/
+/*		SDK.															*/
 /*																		*/
 /************************************************************************/
 /*  Revision History:													*/
 /*																		*/
-/*	07/21/2010(AaronO): created											*/
+/*	03/02/2010(AaronO): created											*/
 /*																		*/
 /************************************************************************/
 
+#define	_CRT_SECURE_NO_WARNINGS
 
 /* ------------------------------------------------------------ */
 /*					Include File Definitions					*/
@@ -38,170 +39,336 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <stdafx.h>
 #include <string.h>
-//#include "pch.h"
 
-
-#include "digilent.adept.sdk_v2.4.2/include/dpcdecl.h"
-#include "digilent.adept.sdk_v2.4.2/include/dmgr.h"
-#include "digilent.adept.sdk_v2.4.2/include/depp.h"
-#include "digilent.adept.sdk_v2.4.2/include/daci.h"
+#include "dpcdecl.h" 
+#include "depp.h"
+#include "dmgr.h"
 
 	/* ------------------------------------------------------------ */
 	/*					Local Type and Constant Definitions			*/
 	/* ------------------------------------------------------------ */
 
+const int cchSzLen = 1024;
+const int cbBlockSize = 1000;
 
-	/* ------------------------------------------------------------ */
-	/*					Global Variables							*/
-	/* ------------------------------------------------------------ */
+/* ------------------------------------------------------------ */
+/*					Global Variables							*/
+/* ------------------------------------------------------------ */
 
+BOOL			fGetReg;
+BOOL			fPutReg;
+BOOL			fGetRegRepeat;
+BOOL			fPutRegRepeat;
+BOOL			fDvc;
+BOOL			fFile;
+BOOL			fCount;
+BOOL			fByte;
 
-	/* ------------------------------------------------------------ */
-	/*					Local Variables								*/
-	/* ------------------------------------------------------------ */
-
-	// Device handle
-HIF hif = hifInvalid;
-
-// Device name string
 char szDvc[64] = "CmodS6";
 
-// Variables to be set by Daci functions
-ULONG bdrSet;
-ULONG cchRcv;
 
-// Strings to send and recieve
-char szSend[] = "Digilent test message";
-char szRcv[1024];
+char			szAction[cchSzLen];
+char			szRegister[cchSzLen];
+//char			szDvc[cchSzLen];
+char			szFile[cchSzLen];
+char			szCount[cchSzLen];
+char			szByte[cchSzLen];
 
-// UART configuration
-ULONG	bdrReq = 2400;
-INT32	cbtData = 8;
-INT32	idStop = 1;
-INT32	idParity = 0;
-BOOL	fRxBlock = fTrue;
+HIF				hif = hifInvalid;
 
-using namespace std;
+FILE* fhin = NULL;
+FILE* fhout = NULL;
 
 
+/* ------------------------------------------------------------ */
+/*				Local Variables									*/
+/* ------------------------------------------------------------ */
 
 
 /* ------------------------------------------------------------ */
 /*					Forward Declarations						*/
 /* ------------------------------------------------------------ */
-void ErrorExit();
+
+BOOL		FParseParam(int cszArg, char* rgszArg[]);
+void		ShowUsage(char* sz);
+BOOL		FInit();
+void		ErrorExit();
+
+void		DoDvcTbl();
+void		DoPutReg();
+BYTE		DoGetReg(BYTE idReg[]);
+void		DoPutRegRepeat();
+void		DoGetRegRepeat();
+
+void		StrcpyS(char* szDst, size_t cchDst, const char* szSrc);
 
 /* ------------------------------------------------------------ */
-/*					Procedure Definitions						*/
+/*				Procedure Definitions							*/
 /* ------------------------------------------------------------ */
 /***	main
 **
-**	Parameters:
+**	Synopsis
+**		int main(cszArg, rgszArg)
+**
+**	Input:
+**		cszArg		- count of command line arguments
+**		rgszArg		- array of command line arguments
+**
+**	Output:
 **		none
 **
+**	Errors:
+**		Exits with exit code 0 if successful, else non-zero
+**
+**	Description:
+**		main function of DEPP Demo application.
+*/
+
+int main(int cszArg, char* rgszArg[]) {
+
+	//if (!FParseParam(cszArg, rgszArg)) {
+	//	ShowUsage(rgszArg[0]);
+	//	return 1;
+	//}
+
+	// DMGR API Call: DmgrOpen
+	if (!DmgrOpen(&hif, szDvc)) {
+		printf("DmgrOpen failed (check the device name you provided)\n");
+		return 0;
+	}
+
+	// DEPP API call: DeppEnable
+	if (!DeppEnable(hif)) {
+		printf("DeppEnable failed\n");
+		return 0;
+	}
+
+	BYTE request_bit = {00000000};
+	BYTE * bit_storage = 0;
+	DWORD value = 0;
+	char * choice = NULL;
+
+
+	// This is for getting the registers forever
+	while (true)
+	{
+		printf("Please enter the values of bits you'd like to find!");
+		
+		scanf("%c", choice);
+
+		if (choice == 'q')
+		{
+			return 0;
+		}
+
+		if (choice != 'q')
+		{
+			request_bit = BYTE(choice);
+		}
+
+
+		if (fGetReg) {
+			DeppGetReg(hif, request_bit, bit_storage, value);						/* Get single byte from register */
+		}
+
+	}
+
+	return 0;
+
+}
+
+
+/* ------------------------------------------------------------ */
+/***	FParseParam
+**
+**	Parameters:
+**		cszArg		- number of command line arguments
+**		rgszArg		- array of command line argument strings
+**
 **	Return Value:
-**		0 if successful
-**		non-zero otherwise
+**		none
+**
+**	Errors:
+**		Returns fTrue if not parse errors, fFalse if command line
+**		errors detected.
+**
+**	Description:
+**		Parse the command line parameters and set global variables
+**		based on what is found.
+*/
+
+BOOL FParseParam(int cszArg, char* rgszArg[]) {
+
+	int		iszArg;
+
+	/* Initialize default flag values */
+	fGetReg = fFalse;
+	fPutReg = fFalse;
+	fGetRegRepeat = fFalse;
+	fPutRegRepeat = fFalse;
+	fDvc = fFalse;
+	fFile = fFalse;
+	fCount = fFalse;
+	fByte = fFalse;
+
+	// Ensure sufficient paramaters. Need at least program name, action flag, register number
+	if (cszArg < 3) {
+		return fFalse;
+	}
+
+	/* The first parameter is the action to perform. Copy the
+	** the first parameter into the action string.
+	*/
+	StrcpyS(szAction, cchSzLen, rgszArg[1]);
+
+	if (strcmp(szAction, "-g") == 0) {
+		fGetReg = fTrue;
+	}
+	else if (strcmp(szAction, "-p") == 0) {
+		fPutReg = fTrue;
+	}
+	else if (strcmp(szAction, "-s") == 0) {
+		fGetRegRepeat = fTrue;
+	}
+	else if (strcmp(szAction, "-l") == 0) {
+		fPutRegRepeat = fTrue;
+	}
+	else { // unrecognized action
+		return fFalse;
+	}
+
+	/* Second paramater is target register on device. Copy second
+	** paramater to the register string */
+	StrcpyS(szRegister, cchSzLen, rgszArg[2]);
+
+
+	/* Parse the command line parameters.
+	*/
+	iszArg = 3;
+	while (iszArg < cszArg) {
+
+		/* Check for the -d parameter which is used to specify
+		** the device name of the device to query.
+		*/
+		if (strcmp(rgszArg[iszArg], "-d") == 0) {
+			iszArg += 1;
+			if (iszArg >= cszArg) {
+				return fFalse;
+			}
+			StrcpyS(szDvc, cchSzLen, rgszArg[iszArg++]);
+			fDvc = fTrue;
+		}
+
+		/* Check for the -f parameter used to specify the
+		** input/output file name.
+		*/
+		else if (strcmp(rgszArg[iszArg], "-f") == 0) {
+			iszArg += 1;
+			if (iszArg >= cszArg) {
+				return fFalse;
+			}
+			StrcpyS(szFile, cchSnMax, rgszArg[iszArg++]);
+			fFile = fTrue;
+		}
+
+		/* Check for the -c parameter used to specify the
+		** number of bytes to read/write from file.
+		*/
+		else if (strcmp(rgszArg[iszArg], "-c") == 0) {
+			iszArg += 1;
+			if (iszArg >= cszArg) {
+				return fFalse;
+			}
+			StrcpyS(szCount, cchUsrNameMax, rgszArg[iszArg++]);
+			fCount = fTrue;
+		}
+
+		/* Check for the -b paramater used to specify the
+		** value of a single data byte to be written to the register
+		*/
+		else if (strcmp(rgszArg[iszArg], "-b") == 0) {
+			iszArg += 1;
+			if (iszArg >= cszArg) {
+				return fFalse;
+			}
+			StrcpyS(szByte, cchUsrNameMax, rgszArg[iszArg++]);
+			fByte = fTrue;
+		}
+
+		/* Not a recognized parameter
+		*/
+		else {
+			return fFalse;
+		}
+	} // End while
+
+	/* Input combination validity checks
+	*/
+	if (!fDvc) {
+		printf("Error: No device specified\n");
+		return fFalse;
+	}
+	if (fPutReg && !fByte) {
+		printf("Error: No byte value provided\n");
+		return fFalse;
+	}
+	if ((fGetRegRepeat || fPutRegRepeat) && !fFile) {
+		printf("Error: No filename provided\n");
+		return fFalse;
+	}
+
+	return fTrue;
+
+}
+
+/* ------------------------------------------------------------ */
+/***	ShowUsage
+**
+**	Synopsis
+**		VOID ShowUsage(sz)
+**
+**	Input:
+**		szProgName	- program name as called
+**
+**	Output:
+**		none
 **
 **	Errors:
 **		none
 **
 **	Description:
-**		DaciDemo main function
+**		prints message to user detailing command line options
 */
-int main(void) {
 
-	/* Clear recieve string buffer */
-	memset(szRcv, 0, 1024);
+void ShowUsage(char* szProgName) {
 
-	/* Connect to device using DMGR */
-	// DMGR API Call: DmgrOpen
-	if (!DmgrOpen(&hif, szDvc)) {
-		printf("Error: Could not open device %s\n", szDvc);
-		ErrorExit();
-	}
+	printf("\nDigilent DEPP demo\n");
+	printf("Usage: %s <action> <register> -d <device name> [options]\n", szProgName);
 
-	/* Enable asynchronous communications interface (UART) on device */
-	// DACI API Call: DaciEnable
-	if (!DaciEnable(hif)) {
-		printf("Error: DtwiEnable failed\n");
-		ErrorExit();
-	}
+	printf("\n\tActions:\n");
+	printf("\t-g\t\t\t\tGet register byte\n");
+	printf("\t-p\t\t\t\tPut Register byte\n");
+	printf("\t-l\t\t\t\tStream file into register\n");
+	printf("\t-s\t\t\t\tStream register into file\n");
 
-	/* Configure baud rate on board */
-	// DACI API Call: DaciSetBaud
-	if (!DaciSetBaud(hif, bdrReq, &bdrSet)) {
-		printf("Error: DaciSetBaud failed\n");
-		ErrorExit();
-	}
+	printf("\n\tOptions:\n");
+	printf("\t-f <filename>\t\t\tSpecify file name\n");
+	printf("\t-c <# bytes>\t\t\tNumber of bytes to read/write\n");
+	printf("\t-b <byte>\t\t\tValue to load into register\n");
 
-	/* Verify that the baud rate was set correctly */
-	if (bdrSet != bdrReq) {
-		printf("WARNING: Baud rate set was different from requested\n");
-		printf("Requested: %lu\n", bdrReq);
-		printf("Set: %lu\n", bdrSet);
-	}
-
-	/* Configure UART frame */
-	// DACI API Call: DaciSetMode
-	if (!DaciSetMode(hif, cbtData, idStop, idParity)) {
-		printf("Error: DaciSetMode failed\n");
-		ErrorExit();
-	}
-
-	/* Make recieve functions blocking. This allows all data sent by the transmitter to arrive before the DaciGetBuf call returns. */
-	// DACI API Call: DaciSetRxBlock
-	if (!DaciSetRxBlock(hif, fTrue)) {
-		printf("Error: DaciSetRxBlock failed\n");
-		ErrorExit();
-	}
-
-	/* Send data to board for UART transmission */
-	// DACI API Call: DaciPutBuf
-	if (!DaciPutBuf(hif, (BYTE*)szSend, strlen(szSend), fFalse)) {
-		printf("Error: DaciPutBuf failed\n");
-		ErrorExit();
-	}
-
-	/* Get recieved UART data. Note that without the previous call to DaciSetRxBlock, this call will fail because
-	   insufficient time will have elapsed from when DaciPutBuf was called for any data to have arrived at the reciever */
-	   // DACI API Call: DaciGetBuf
-	if (!DaciGetBuf(hif, (BYTE*)szRcv, strlen(szSend), &cchRcv, fFalse)) {
-		printf("Error: DaciGetBuf failed\n");
-		ErrorExit();
-	}
-
-
-	/* Compare recieved data to transmitted data */
-	if (strcmp(szSend, szRcv) == 0) {
-		printf("Recieved data matched transmitted data. Success\n");
-	}
-	else {
-		printf("Error: Recieved string was different from send string.\n");
-		printf("Sent: %s\n", szSend);
-		printf("Recieved: %s\n", szRcv);
-	}
-
-
-	// DACI API Call: DaciDisable
-	DaciDisable(hif);
-
-	// DMGR API Call: DmgrClose
-	DmgrClose(hif);
-
-
-	return 0;
+	printf("\n\n");
 }
 
 
 
 /* ------------------------------------------------------------ */
-/***	ErrorExit
+/***	StrcpyS
 **
 **	Parameters:
-**		none
+**		szDst - pointer to the destination string
+**		cchDst - size of destination string
+**		szSrc - pointer to zero terminated source string
 **
 **	Return Value:
 **		none
@@ -210,21 +377,31 @@ int main(void) {
 **		none
 **
 **	Description:
-**		Disables Daci, closes the device, and exits the program
+**		Cross platform version of Windows function strcpy_s.
 */
-void ErrorExit() {
-	if (hif != hifInvalid) {
-		// DACI API Call: DaciDisable
-		DaciDisable(hif);
+void StrcpyS(char* szDst, size_t cchDst, const char* szSrc) {
 
-		// DMGR API Call: DmgrClose
-		DmgrClose(hif);
+#if defined (WIN32)
+
+	strcpy_s(szDst, cchDst, szSrc);
+
+#else
+
+	if (0 < cchDst) {
+
+		strncpy(szDst, szSrc, cchDst - 1);
+		szDst[cchDst - 1] = '\0';
 	}
 
-	exit(1);
+#endif
 }
 
 /* ------------------------------------------------------------ */
 
 /************************************************************************/
+
+
+
+
+
 
