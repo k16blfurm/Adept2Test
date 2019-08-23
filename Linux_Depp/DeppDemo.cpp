68195824e39c48485c73ada366d870cb0f236bab
Blaine Furman <bfurman@hawaii.edu>
2019-08-22 21:32:20 -1000

    Fixed DeppDemo File and cleaned folder
    I made the DeppDemo file robust and changed the compile program from gcc to g++ because of compile time errors with libraries.

--- Linux_Depp/DeppDemo.cpp
+++ Linux_Depp/DeppDemo.cpp
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <fstream>
 #include "dpcdecl.h" 
 #include "dmgr.h"
@@ -21,6 +22,10 @@
 #define OP1ADDR         0x00
 #define OP2ADDR         0x01
 #define RESADDR         0x00
+
+//function declaration
+
+int is_valid(char letter);
 
 int main()
 {
@@ -31,13 +36,21 @@ int main()
     bool test = true;  
     bool get_reg = true;
     int reg;
-    bool getting_reg = true;
+    char getting_reg;
+    bool FinishGettingReg = false; // this is for checking if getting the register is finished
     BYTE* stream;
     char choice;
     bool restart = false;
     char value;
+    int finished_reg; // this is for checking if the user is done or not
     // stream for saving to file
-    std::ofstream file;
+    //std::ofstream file;
+    FILE * pFile;
+
+
+    // this opens the file registers, let the user make their own names in new releases
+    pFile = fopen ("registers", "wb");
+    //file.open("registers");
 
 
     //opening test for device
@@ -62,7 +75,7 @@ int main()
             std::cout<<"Enter in the addresses you'd like";
             std::cin>> reg;
             if(isalpha(reg))
-            {    reg = NULL; 
+            {    reg = 0; 
                  get_reg == true;
                 std::cout<<"enter the address in again, it is invalid"<<std::endl;
             }
@@ -74,21 +87,26 @@ int main()
 
         std::cout<<"type l to stop saving data"<<std::endl;
 
-        while(getting_reg == true)
+        while(FinishGettingReg == false)
         {   // This is the repeat to a stream register
             DeppGetRegRepeat(deviceHandle, reg, stream, 10, true);
+            // this is to save to a file before going to next run
+            fwrite(stream,sizeof(BYTE),10,pFile);
             //std::cout<<"Reg :"<<reg<<std::cout<<"stream"
             std::cin>>getting_reg;
-            if(getting_reg == 'q')
+            finished_reg = is_valid(getting_reg); 
+            if(finished_reg == 1)
                 return 0;
-            if(getting_reg == 'l')
-                getting_reg = false;
+            if(finished_reg == 2)
+                FinishGettingReg = true;
         }
 
         while(restart == false)
         {
         std::cout<<"Would you like clear stream and save values or restart?"<<std::endl;
-        std::cout<<"Enter r to restart, s to save";
+        std::cout<<"Enter r to restart, s to save"<<std::endl;
+        std::cin>> choice;
+        
         if(choice == 'r')
             restart = true;
         if(choice == 's')
@@ -100,14 +118,13 @@ int main()
             std::cout<<"Sorry, the program couldn't understand your choice"<<std::endl;
             std::cout<<"Please enter your choice again..."<<std::endl;
         }
-        choice = NULL;
+
+        
         }
     }
 
 // Saving to a file
-    file.open("registers.txt");
-    file<<stream;
-    file.close();
+    fclose(pFile);
 
 if (status)
   printf("Successfully closed device handle\n");
@@ -118,4 +135,17 @@ else
 } 
     return 0;
     
+}
+
+// This function is for checking if a char is a valid entry
+int is_valid(char letter)
+{
+        switch(letter)
+        {
+            case 'q': return 1;
+            case 'l': return 2;
+            default: return false;
+
+        }
+
 }
